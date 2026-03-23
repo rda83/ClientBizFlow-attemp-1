46cb4d1e48e417685d5a8a066bbbcd1051f5386b
Denis Rostovtsev <rda83@mail.ru>
2026-03-23 09:32:51 +0700

    + IBizFlowStorage.GetJournalRecordByLaunchIdAsync

--- BizFlowJournal.cs
+++ BizFlowJournal.cs
@@ -1,5 +1,4 @@
 ﻿using BizFlow.Core.Contracts;
-using BizFlow.Core.Model;
 using ClientBizFlow_attemp_1.Database;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,35 +10,6 @@ namespace ClientBizFlow_attemp_1
         public BizFlowJournal(AppDbContext context)
         {
             _context = context;
-        }
-
-        public async Task<IEnumerable<JournalRecord>> GetJournalRecordByLaunchId(string launchId,
-            CancellationToken cancellationToken = default)
-        {
-            if (string.IsNullOrWhiteSpace(launchId))
-                throw new ArgumentException("Необходимо передать идентификатор запуска пайплайна", nameof(launchId));
-
-            var resut = await _context.BizFlowJournalRecords
-                .Where(i => i.LaunchId == launchId)
-                .AsNoTracking()
-                .ToListAsync(cancellationToken);
-
-            var dtos = resut.Select(r => new JournalRecord
-            {
-                Period = r.Period,
-                PipelineName = r.PipelineName,
-                ItemDescription = r.ItemDescription,
-                ItemSortOrder = r.ItemSortOrder,
-                TypeAction = r.TypeAction,
-                TypeOperationId = r.TypeOperationId,
-                LaunchId = r.LaunchId,
-                Message = r.Message,
-                Trigger = r.Trigger,
-                IsStartNow = r.IsStartNowPipeline,
-                ItemId = r.ItemId,
-            }).ToList();
-
-            return dtos;
         }
 
         public async Task<string?> GetLastLaunchId(string pipelineName,
